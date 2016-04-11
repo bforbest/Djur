80fb1c9a8a2a11eac53be480ad30403be1f13837
ali sina <ali@mra.li>
2016-04-11 10:34:45 +0200

    Admin

---
+++ Djur/Djur/Controllers/SearchController.cs
@@ -0,0 +1,28 @@
+﻿using Djur.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Djur.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+
+        public ActionResult Index(string SearchString)
+        {
+            if (!String.IsNullOrEmpty(SearchString))
+            {
+                List<Product> products = (List<Product>)Session["lista"];
+                var items = products.Where(x => x.Title.Contains(SearchString));
+                return View(items);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
