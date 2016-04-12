8cf9b1d357a73dbf4bc15f76696367480ccff2c6
Sara <sara.szajewska@gmail.com>
2016-04-12 15:39:53 +0200

    Design

--- Djur/Djur/Global.asax.cs
+++ Djur/Djur/Global.asax.cs
@@ -20,18 +20,18 @@ namespace Djur
             Session["LoginStatus"] = false;
             Session["Admin"] = false;
             Session["lista"] = new List<Product>() {
-                new Product() {ProductID=1,  Title = "English Shepherd", Amount = 1, ImageUrl="http://yupdaily.com/wp-content/uploads/2015/10/kfuh-kuvasz.jpg", Price = 7000, Category = "Dogs" },
-                new Product() { ProductID=2,Title = "Kuvasz", Amount = 1,  ImageUrl="https://englishshepherd.files.wordpress.com/2010/03/picture-008-bess-5x7.jpg", Price = 300 },
-                new Product() { ProductID=3,Title = "Siberian Husky", Amount = 1,  ImageUrl="http://www.dogtemperament.com/wp-content/uploads/2013/02/siberian-husky.jpg", Price = 300 },
-                new Product() { ProductID=4,Title = "Teacup Pomeranian", Amount = 1, ImageUrl="https://s-media-cache-ak0.pinimg.com/736x/2b/8d/0f/2b8d0fe811c08dc3eb9f03b6668f6522.jpg", Price = 300 },
-                new Product() { ProductID=5,Title = "American Bobtail", Amount = 1,  ImageUrl="http://cdn1-www.cattime.com/assets/uploads/2011/12/file_2738_american-bobtail-460x290.jpg", Price = 300 },
+                new Product() {ProductID=1,  Title = "English Shepherd", Amount = 1, ImageUrl="http://www.resizr.com/resized/0216.jpg", Price = 7000, Category = "Dogs" },
+                new Product() { ProductID=2,Title = "Kuvasz", Amount = 1,  ImageUrl="http://yupdaily.com/wp-content/uploads/2015/10/kfuh-kuvasz.jpg", Price = 300, Category = "Dogs" },
+                new Product() { ProductID=3,Title = "Siberian Husky", Amount = 1,  ImageUrl="http://www.dogtemperament.com/wp-content/uploads/2013/02/siberian-husky.jpg", Price = 300, Category = "Dogs" },
+                new Product() { ProductID=4,Title = "Teacup Pomeranian", Amount = 1, ImageUrl="https://s-media-cache-ak0.pinimg.com/736x/2b/8d/0f/2b8d0fe811c08dc3eb9f03b6668f6522.jpg", Price = 300, Category="Dogs" },
+                new Product() { ProductID=5,Title = "American Bobtail", Amount = 1,  ImageUrl="http://cdn1-www.cattime.com/assets/uploads/2011/12/file_2738_american-bobtail-460x290.jpg", Price = 300, Category="Cats" },
                 new Product() { ProductID=6,Title = "Bengal", Amount = 1,  ImageUrl="http://www.bengalcatworld.com/home/wp-content/uploads/2012/02/bengal-cats-and-kittens-028.jpg", Price = 300, Category="Cats" },
-                new Product() { ProductID=7,Title = "Chartreux", Amount = 1, ImageUrl="https://s-media-cache-ak0.pinimg.com/236x/7b/6d/8e/7b6d8e231551bc6b175ab38098047b2e.jpg", Price = 300, Category="Cats" },
+                new Product() { ProductID=7,Title = "Chartreux", Amount = 1, ImageUrl="http://www.catbreedsjunction.com/images/chartreux-cat-breed.jpg", Price = 300, Category="Cats" },
                 new Product() { ProductID=8,Title = "Norwegian Forest Cat", Amount = 1, ImageUrl="https://upload.wikimedia.org/wikipedia/commons/6/6e/Norskskogkatt_Evita_3.JPG", Price = 300, Category="Cats" },
-                new Product() { ProductID=8,Title = "American Fuzzy Lop", Amount = 1,  ImageUrl="https://rabbitsforsaleinidaho.files.wordpress.com/2014/12/205.jpg?w=640", Price = 300, Category="Rabbits" },
-                new Product() { ProductID=8,Title = "Angora", Amount = 1, ImageUrl="http://4.bp.blogspot.com/-QPk86FZ87iM/TpI0jWi-WlI/AAAAAAAABeU/T1L1-UmNHIc/s1600/DSC02485.JPG", Price = 300, Category="Rabbits" },
-                new Product() { ProductID=8,Title = "Giant Papillon", Amount = 1, ImageUrl="http://vetbook.org/wiki/rabbit/images/4/42/Giant_Papillon.jpg", Price = 300, Category="Rabbits" },
-                new Product() { ProductID=8,Title = "Czech Red", Amount = 1,  ImageUrl="https://s-media-cache-ak0.pinimg.com/736x/20/52/99/205299943204c6aa0826012f83b3c416.jpg", Price = 300, Category="Rabbits" }
+                new Product() { ProductID=9,Title = "Fuzzy Lop", Amount = 1,  ImageUrl="https://rabbitsforsaleinidaho.files.wordpress.com/2014/12/205.jpg?w=640", Price = 300, Category="Rabbits" },
+                new Product() { ProductID=10,Title = "Angora", Amount = 1, ImageUrl="http://4.bp.blogspot.com/-QPk86FZ87iM/TpI0jWi-WlI/AAAAAAAABeU/T1L1-UmNHIc/s1600/DSC02485.JPG", Price = 300, Category="Rabbits" },
+                new Product() { ProductID=11,Title = "Giant Papillon", Amount = 1, ImageUrl="http://vetbook.org/wiki/rabbit/images/4/42/Giant_Papillon.jpg", Price = 300, Category="Rabbits" },
+                new Product() { ProductID=12,Title = "Czech Red", Amount = 1,  ImageUrl="https://s-media-cache-ak0.pinimg.com/736x/20/52/99/205299943204c6aa0826012f83b3c416.jpg", Price = 300, Category="Rabbits" }
             };
             Session["ShoppingCartList"] = new ShoppingCart() { };
         }
